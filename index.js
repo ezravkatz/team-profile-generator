bf88b1837a047d1d060dc3cdd0864b63d136323d
ezravkatz <ezravkatz@gmail.com>
2022-08-15 23:45:42 -0400

    added classes for employee, engineer, intern, and manager

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
-const generateProfile = require("./src/generateProfile");
+const generateProfile = require('./src/generateProfile');
 
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-const fs = require("fs");
-const inquirer = require("inquirer");
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 const teamProfileArray = [];
 
 const managerInfo = () => {
   return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Please enter the name of the manager of this team.",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("Error! Please enter the manager's name.");
-            return false;
-          }
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Please enter the name of the manager of this team.',
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Error! Please enter the manager\'s name.');
+              return false;
+            }
+          },
         },
-      },
-      {
-        type: "input",
-        name: "ID",
-        message: "Please enter the manager's ID.",
-        validate: (nameInput) => {
-          if (isNaN(nameInput)) {
-            console.log("Error! Please enter the manager's ID.");
-            return false;
-          } else {
-            return true;
-          }
+        {
+          type: 'input',
+          name: 'ID',
+          message: 'Please enter the manager\'s ID.',
+          validate: (nameInput) => {
+            if (isNaN(nameInput)) {
+              console.log('Error! Please enter the manager\'s ID.');
+              return false;
+            } else {
+              return true;
+            }
+          },
         },
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Please enter the manager's email.",
-        validate: (email) => {
-          valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-          if (valid) {
-            return true;
-          } else {
-            console.log("Error! Please enter the manager's email address.");
-            return false;
-          }
+        {
+          type: 'input',
+          name: 'email',
+          message: 'Please enter the manager\'s email.',
+          validate: (email) => {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+            if (valid) {
+              return true;
+            } else {
+              console.log('Error! Please enter the manager\'s email address.');
+              return false;
+            }
+          },
         },
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "Please enter the manager's office number.",
-        validate: (nameInput) => {
-          if (isNaN(nameInput)) {
-            console.log("Error! Please enter the manager's office number.");
-            return false;
-          } else {
-            return true;
-          }
+        {
+          type: 'input',
+          name: 'officeNumber',
+          message: 'Please enter the manager\'s office number.',
+          validate: (nameInput) => {
+            if (isNaN(nameInput)) {
+              console.log('Error! Please enter the manager\'s office number.');
+              return false;
+            } else {
+              return true;
+            }
+          },
         },
-      },
-    ])
-    .then((managerProfile) => {
-      const { name, ID, email, OfficeNumber } = managerProfile;
-      const manager = new Manager(name, ID, email, OfficeNumber);
+      ])
+      .then((managerProfile) => {
+        const {name, ID, email, OfficeNumber} = managerProfile;
+        const manager = new Manager(name, ID, email, OfficeNumber);
 
-      teamProfileArray.push(manager);
-      console.log(manager);
-    });
+        teamProfileArray.push(manager);
+        console.log(manager);
+      });
 };
 
 const employeeInfo = () => {
@@ -81,135 +81,131 @@ const employeeInfo = () => {
   `);
 
   return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "positon",
-        message: "Please indicate the position of the employee.",
-        options: ("Engineer", "Intern"),
-      },
-      {
-        type: "input",
-        name: "name",
-        message: "Please enter the employee's name.",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("Error! Please enter a name for the employee's entry.");
-            return false;
-          }
+      .prompt([
+        {
+          type: 'input',
+          name: 'positon',
+          message: 'Please indicate the position of the employee.',
+          options: ('Engineer', 'Intern'),
         },
-      },
-      {
-        type: "input",
-        name: "ID",
-        message: "Please enter the employee's ID number.",
-        validate: (nameInput) => {
-          if (isNaN(nameInput)) {
-            console.log("Error! Please enter a name for the employee entry.");
-            return false;
-          } else {
-            return true;
-          }
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Please enter the employee name.',
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Error! Please enter the employee name.');
+              return false;
+            }
+          },
         },
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Please enter the employee's email address.",
-        validate: (email) => {
-          valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-          if (valid) {
-            return true;
-          } else {
-            console.log(
-              "Error! Please enter a valid email address for the employee entry."
-            );
-            return false;
-          }
+        {
+          type: 'input',
+          name: 'ID',
+          message: 'Please enter the employee ID number.',
+          validate: (nameInput) => {
+            if (isNaN(nameInput)) {
+              console.log('Error! Please enter an ID number for the employee.');
+              return false;
+            } else {
+              return true;
+            }
+          },
         },
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "Please enter the employee's GitHub username.",
-        when: (input) => input.position === "Engineer",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log(
-              "Error! Please provide the emmployee's github username."
-            );
-          }
+        {
+          type: 'input',
+          name: 'email',
+          message: 'Please enter the employee\'s email address.',
+          validate: (email) => {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+            if (valid) {
+              return true;
+            } else {
+              console.log('Error! Please enter a valid email address.');
+              return false;
+            }
+          },
         },
-      },
-      {
-        type: "input",
-        name: "school",
-        message:
-          "Please enter the name of the school your intern is affiliated with!",
-        when: (input) => input.position === "Intern",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("Error! Please enter the school name.");
-            return false;
-          }
+        {
+          type: 'input',
+          name: 'github',
+          message: 'Please enter the employee\'s GitHub username.',
+          when: (input) => input.position === 'Engineer',
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log(
+                  'Error! Please provide the emmployee\'s github username.',
+              );
+            }
+          },
         },
-      },
-      {
-        type: "confirmation",
-        name: "confirmAdd",
-        message: "Do you want to add another member to your team?",
-        default: false,
-      },
-    ])
-    .then((teamData) => {
-      let { name, ID, email, position, github, school, confirmAdd } = teamData;
-      let employee;
+        {
+          type: 'input',
+          name: 'school',
+          message:
+          'Please enter the name of the school your intern is affiliated with!',
+          when: (input) => input.position === 'Intern',
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Error! Please enter the school name.');
+              return false;
+            }
+          },
+        },
+        {
+          type: 'confirmation',
+          name: 'confirmAdd',
+          message: 'Do you want to add another member to your team?',
+          default: false,
+        },
+      ])
+      .then((teamData) => {
+    const {name, ID, email, position, github, school, confirmAdd} = teamData;
+        let employee;
 
-      if (position === "Engineer") {
-        employee = new Engineer(name, ID, email, github);
-        console.log(employee);
-      } else if (position === "Intern") {
-        employee = new Intern(name, ID, email, school);
-        console.log(employee);
-      }
+        if (position === 'Engineer') {
+          employee = new Engineer(name, ID, email, github);
+          console.log(employee);
+        } else if (position === 'Intern') {
+          employee = new Intern(name, ID, email, school);
+          console.log(employee);
+        }
 
-      teamProfileArray.push(employee);
+        teamProfileArray.push(employee);
 
-      if (confirmAdd) {
-        return employeeInfo(teamProfileArray);
-      } else {
-        return teamProfileArray;
-      }
-    });
+        if (confirmAdd) {
+          return employeeInfo(teamProfileArray);
+        } else {
+          return teamProfileArray;
+        }
+      });
 };
 
 const createFile = (data) => {
-  fs.createFile("./dist/index.html", data, (err) => {
+  fs.createFile('./dist/index.html', data, (err) => {
     if (err) {
       console.log(err);
       return;
     } else {
-      console.log(
-        "Your team profile has been generated! You can find it by checking the index.html file."
-      );
+      console.log('Team profile generated! Please check the index.html file.');
     }
   });
 };
 
 managerInfo()
-  .then(employeeInfo)
-  .then((teamProfileArray) => {
-    return generateProfile(teamProfileArray);
-  })
-  .then((pageDesign) => {
-    return createFile(pageDesign);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .then(employeeInfo)
+    .then((teamProfileArray) => {
+      return generateProfile(teamProfileArray);
+    })
+    .then((pageDesign) => {
+      return createFile(pageDesign);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
--- src/generateProfile.js
+++ src/generateProfile.js
@@ -1,15 +1,15 @@
 const createManager = function(manager) {
   return `
-  <div className="col-4 mt-4">
-    <div className="card h-80">
+  <div>
+    <div>
       <div className="card-head">
         <h2>${manager.name}</h2>
         <h3>Manager</h3><i class="fa-solid fa-briefcase"></i>
       </div>
       <div className="card-body">
-        <p className="ID">ID: ${manager.id}</p>
+        <p>ID: ${manager.id}</p>
         <p className="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-        <p className="">Office Number: ${manager.officeNumber}</p>
+        <p>Office Number: ${manager.officeNumber}</p>
       </div>
     </div>
   </div>
@@ -18,14 +18,14 @@ const createManager = function(manager) {
 
 const createEngineer = function(engineer) {
   return `
-  <div class="col-4 mt-4">
-  <div class="card h-80">
+  <div>
+  <div class="card">
       <div class="card-head">
           <h2>${engineer.name}</h2>
           <h3>Engineer</h3><i class="fa-solid fa-laptop"></i>
       </div>
       <div class="card-body">
-          <p class="ID">ID: ${engineer.id}</p>
+          <p>ID: ${engineer.id}</p>
           <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
           <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
       </div>
@@ -36,16 +36,16 @@ const createEngineer = function(engineer) {
 
 const createIntern = function(intern) {
   return `
-  <div class="col-4 mt-4">
-  <div class="card h-80">
+  <div>
+  <div class="card">
       <div class="card-head">
           <h2>${intern.name}</h2>
           <h3>Intern</h3><i class="fa-solid fa-graduation-cap"></i>
       </div>
       <div class="card-body">
-          <p class="ID">ID: ${intern.id}</p>
+          <p>ID: ${intern.id}</p>
           <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
-          <p class="school">Attending: ${intern.school}</p>
+          <p>Attending: ${intern.school}</p>
       </div>
 </div>
 </div>
@@ -95,13 +95,13 @@ const createTeamProfile = function(teamCards) {
 </head>
 <body>
     <header>
-        <nav class="navbar" id="navbar">
-            <span class="navbar-brand mb-0 h1 w-80 text-center" id="navBarText">Team Profile</span>
+        <nav class="navbar">
+            <span id="navBarText">Team Profile</span>
         </nav>
     </header>
     <main>
-        <div class="container">
-            <div class="row justify-content-center" id="teamCards">
+        <div id="cardContainer">
+            <div id="teamCards">
                 <!--Team Cards-->
                 ${teamCards}}
             </div>
